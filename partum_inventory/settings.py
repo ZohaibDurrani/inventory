9a96a6998fdaef4095a48a0b14442abab2472dfc
ZohaibDurrani <zkdurrani11@gmail.com>
2022-07-30 17:23:31 +0500

    mais

--- partum_inventory/settings.py
+++ partum_inventory/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,10 +138,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "app_static"),
-    '/var/www/static/',
+    # '/var/www/static/',
 ]
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
 
+django_heroku.settings(locals())
